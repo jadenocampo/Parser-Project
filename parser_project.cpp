eff81e917cf0bc1d5fbef043ca67bb2ff3f1e002
jadenocampo <jadenocampo09@gmail.com>
2024-09-26 16:59:27 -0400

    testing input

--- parser_project.cpp
+++ parser_project.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main(){ 
- 
-    return 0; 
+    ifstream inputFile("TestProgram2.txt");
+     
+
+    if (!inputFile.is_open()){
+        cerr << "Error opening file " << endl;
+        return 1;
+    }
+    string line;
+    cout <<"Test Program 2: "<< endl;
+    while (getline(inputFile, line)){
+        cout << line << endl;
+    }
+    inputFile.close();
+    return 0;
 }
