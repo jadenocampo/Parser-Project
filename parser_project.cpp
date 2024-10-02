a032bcc186783eedc78c6f1cacc41dc39f85d5ca
jadenocampo <jadenocampo09@gmail.com>
2024-10-01 22:02:08 -0400

    slight change

--- parser_project.cpp
+++ parser_project.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cctype>
 using namespace std;
+
+
+
 
 int main(){ 
     ifstream inputFile("TestProgram2.txt");
-     
 
     if (!inputFile.is_open()){
         cerr << "Error opening file " << endl;
@@ -14,10 +17,7 @@ int main(){
     string file_line;
     cout <<"Test Program 2: "<< endl;
     while (getline(inputFile, file_line)){
-        for(size_t i =0; i < file_line.length(); i++){
-            cout << file_line[i] << "";
-        }
-        cout << endl;
+        cout << file_line << endl;
     }
     inputFile.close();
     return 0;
